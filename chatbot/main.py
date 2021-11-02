7b820dbe5f7163764b007e384047905ab63d71fc
flavian-koima <53899779+flavian-koima@users.noreply.github.com>
2021-11-02 11:10:17 +0300

    Add files via upload

---
+++ chatbot/main.py
@@ -0,0 +1,69 @@
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+import json
+
+with open("intents.json") as file:
+    data = json.load(file)
+
+words = []
+labels = []
+docs_x = []
+docs_y = []
+
+for intent in data['intents']:
+    for pattern in intent['patterns']:
+        wrds = nltk.word_tokenize(pattern)
+        words.extend(wrds)
+        docs_x.append(wrds)
+        docs_y.append(intent['tag'])
+
+    if intent['tag'] not in labels:
+        labels.append(intent['tag'])
+
+words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+words = sorted(list(set(words)))
+
+labels = sorted(labels)
+
+training = []
+output = []
+
+out_empty = [o for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag = []
+    wrds = [stemmer.stem(x) for w in doc]
+
+    for w in words:
+        if w in words:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row = out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training = np.array(training)
+output = numpy.array(output)
+
+tf.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, n_epoch=100, batch_size=8, show_metric=True)
+model.save('model.tflearn')
